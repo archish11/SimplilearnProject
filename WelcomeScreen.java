0ecbaab1586a6019cba0c2f940671a49b674272a
Archish Pandey <apsibbu@gmail.com>
2021-06-11 16:41:09 +0530

    Adding Welcome Screen including application name developer details aand User interface information

---
+++ WelcomeScreen.java
@@ -0,0 +1,14 @@
+package Lockersproject;
+import java.io.IOException;
+
+public class WelcomeScreen {
+	public static String path = "C:/LockersProject";
+	public static void main(String[] args) throws IOException {
+		System.out.println("WELCOME To VIRTUAL User Interface");
+		System.out.println("Developed By"+ "\nArchish Kumar Pandey\n");
+		MainMenu mainmenu = new MainMenu();
+		mainmenu.method1();                                    
+	        }                      
+        }
+
+
